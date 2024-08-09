482b2c3f9926c4507ab72e1f56036f3d40363b2f
c <fdsatrewq3@gmail.com>
2024-08-09 18:03:41 +0300

    Remade cm W in lua

--- game/scripts/vscripts/addon_init.lua
+++ game/scripts/vscripts/addon_init.lua
@@ -65,6 +65,8 @@ LinkLuaModifier("modifier_shadow_cleric_path_of_darkness_shadow_priest_form", "m
 -- axe: wounding strike
 LinkLuaModifier("modifier_wounding_strike_heal_debuff", "modifiers/heroes/brutal_brawler/wounding_strike/modifier_wounding_strike_heal_debuff", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_wounding_strike_bleed_debuff", "modifiers/heroes/brutal_brawler/wounding_strike/modifier_wounding_strike_bleed_debuff", LUA_MODIFIER_MOTION_NONE)
+-- cm: frost shatter
+LinkLuaModifier("modifier_ice_shatter", "modifiers/heroes/winter_wizard/frost_shatter/modifier_ice_shatter", LUA_MODIFIER_MOTION_NONE)
 -- for lua pets
 LinkLuaModifier("modifier_pet_system_lua", "modifiers/heroes/modifier_pet_system_lua", LUA_MODIFIER_MOTION_NONE)
 -- frostwyrm fury (path talent)
