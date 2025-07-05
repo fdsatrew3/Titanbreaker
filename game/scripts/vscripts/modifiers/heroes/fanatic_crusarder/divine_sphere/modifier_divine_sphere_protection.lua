2ef22d5c96c7d3fc6103aa25ead4fb6e05134f37
c <fdsatrewq3@gmail.com>
2025-07-05 12:54:17 +0300

    ...

--- game/scripts/vscripts/modifiers/heroes/fanatic_crusarder/divine_sphere/modifier_divine_sphere_protection.lua
+++ game/scripts/vscripts/modifiers/heroes/fanatic_crusarder/divine_sphere/modifier_divine_sphere_protection.lua
@@ -12,20 +12,20 @@ modifier_divine_sphere_protection = class({
         return 
         {
             MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
-			MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
-			MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
-			MODIFIER_EVENT_ON_TAKEDAMAGE
+            MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
+            MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
+            MODIFIER_EVENT_ON_TAKEDAMAGE
         }
     end
 })
 
 function modifier_divine_sphere_protection:OnCreated()
     self:OnRefresh()
-	
-	if(not IsServer()) then
-		return
-	end
-	
+    
+    if(not IsServer()) then
+    	return
+    end
+    
     self.parent = self:GetParent()
     self.ability = self:GetAbility()
     
@@ -34,8 +34,8 @@ function modifier_divine_sphere_protection:OnCreated()
     local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_warlock/warlock_shadow_word_buff_c.vpcf", PATTACH_OVERHEAD_FOLLOW, self.parent)
     ParticleManager:SetParticleControlEnt(particle, 0, self.parent, PATTACH_POINT_FOLLOW, "attach_overhead", parentOrigin, true)
     ParticleManager:SetParticleControlEnt(particle, 1, self.parent, PATTACH_POINT_FOLLOW, "attach_overhead", parentOrigin, true)
-	ParticleManager:SetParticleControlEnt(particle, 2, self.parent, PATTACH_POINT_FOLLOW, "attach_overhead", parentOrigin, true)
-	ParticleManager:SetParticleControlEnt(particle, 3, self.parent, PATTACH_POINT_FOLLOW, "attach_overhead", parentOrigin, true)
+    ParticleManager:SetParticleControlEnt(particle, 2, self.parent, PATTACH_POINT_FOLLOW, "attach_overhead", parentOrigin, true)
+    ParticleManager:SetParticleControlEnt(particle, 3, self.parent, PATTACH_POINT_FOLLOW, "attach_overhead", parentOrigin, true)
     self:AddParticle(particle, false, false, 1, false, false)
     
     particle = ParticleManager:CreateParticle("particles/econ/events/ti6/teleport_start_ti6_lvl3_rays.vpcf", PATTACH_CUSTOMORIGIN, self.parent)
@@ -46,25 +46,25 @@ function modifier_divine_sphere_protection:OnCreated()
     particle = ParticleManager:CreateParticle("particles/econ/items/omniknight/hammer_ti6_immortal/omniknight_pur_ti6_immortal_beams.vpcf", PATTACH_CUSTOMORIGIN, self.parent)
     ParticleManager:SetParticleControlEnt(particle, 0, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parentOrigin, true)
     ParticleManager:SetParticleControlEnt(particle, 1, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parentOrigin, true)
-	ParticleManager:SetParticleControlEnt(particle, 2, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parentOrigin, true)
-	ParticleManager:SetParticleControlEnt(particle, 3, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parentOrigin, true)
+    ParticleManager:SetParticleControlEnt(particle, 2, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parentOrigin, true)
+    ParticleManager:SetParticleControlEnt(particle, 3, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parentOrigin, true)
     self:AddParticle(particle, false, false, 1, false, false)
 end
 
 function modifier_divine_sphere_protection:OnRefresh()
     self.ability = self:GetAbility()
-		
-	if(not IsServer()) then
-		return
-	end
-	self.radius = self.ability:GetSpecialValueFor("radius")
-	
-	self:StartIntervalThink(self.ability:GetSpecialValueFor("hammertime"))
+    	
+    if(not IsServer()) then
+    	return
+    end
+    self.radius = self.ability:GetSpecialValueFor("radius")
+    
+    self:StartIntervalThink(self.ability:GetSpecialValueFor("hammertime"))
 end
 
 function modifier_divine_sphere_protection:OnIntervalThink()
-	local enemies = FindNearbyEnemies(self.parent, self.parent:GetAbsOrigin(), self.radius)
-	
+    local enemies = FindNearbyEnemies(self.parent, self.parent:GetAbsOrigin(), self.radius)
+    
     local projectile = {
         Target = nil,
         Source = self.parent,
@@ -77,20 +77,20 @@ function modifier_divine_sphere_protection:OnIntervalThink()
         iVisionTeamNumber = self.parent:GetTeamNumber(),
         iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_HITLOCATION
     }
-	
-	for _, enemy in pairs(enemies) do
-		projectile.Target = enemy
-		ProjectileManager:CreateTrackingProjectile(projectile)
-	end
-	
-	EmitSoundOn("Hero_LegionCommander.PreAttack", self.parent)
+    
+    for _, enemy in pairs(enemies) do
+    	projectile.Target = enemy
+    	ProjectileManager:CreateTrackingProjectile(projectile)
+    end
+    
+    EmitSoundOn("Hero_LegionCommander.PreAttack", self.parent)
 end
 
 function modifier_divine_sphere_protection:OnTakeDamage(kv)
     if(kv.unit ~= self.parent) then
         return
     end
-	
+    
     DivineSphereTakeDamage({
     	DamageTaken = kv.damage,
     	unit = kv.unit,
@@ -100,13 +100,13 @@ function modifier_divine_sphere_protection:OnTakeDamage(kv)
 end
 
 function modifier_divine_sphere_protection:GetAbsoluteNoDamagePhysical()
-	return self:GetStackCount()
+    return self:GetStackCount()
 end
 
 function modifier_divine_sphere_protection:GetAbsoluteNoDamageMagical()
-	return self:GetStackCount()
+    return self:GetStackCount()
 end
 
 function modifier_divine_sphere_protection:GetAbsoluteNoDamagePure()
-	return self:GetStackCount()
+    return self:GetStackCount()
 end
